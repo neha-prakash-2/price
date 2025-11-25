f2efa85f4724aa0418d6885410aed1c867a80bb6
Neha P <110446433+neha-prakash-2@users.noreply.github.com>
2025-11-26 00:36:39 +0530

    Update user_panel.php

--- user_panel.php
+++ user_panel.php
@@ -150,7 +150,7 @@ if ($pdo) {
                         <div class="p-title"><?php echo htmlspecialchars($p['name']); ?></div>
                         
                         <?php if ($p['lowest_price']): ?>
-                            <div class="p-price">From $ ₹ <?php echo number_format($p['lowest_price'], 2); ?></div>
+                            <div class="p-price">From  ₹ <?php echo number_format($p['lowest_price'], 2); ?></div>
                         <?php else: ?>
                             <div class="p-price" style="color: #9ca3af; font-size: 1rem;">Price Unavailable</div>
                         <?php endif; ?>
